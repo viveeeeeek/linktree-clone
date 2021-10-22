395cf1a54aff8aff82126a9ff6a9637678ea81a1
Vivek Sonawane <viveksonawane600@gmail.com>
2021-10-22 19:45:02 +0530

    still in development

--- lib/entities/user_data.entity.dart
+++ lib/entities/user_data.entity.dart
@@ -7,21 +7,19 @@ String userdataToJson(List<UserData> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class UserData {
+  String username;
+  String title;
+  String link;
+
   UserData({
+    required this.username,
     required this.title,
     required this.link,
   });
 
-  String title;
-  String link;
-
   factory UserData.fromJson(Map<String, dynamic> json) => UserData(
-        title: json["title"],
-        link: json["link"],
-      );
+      title: json["title"], link: json["link"], username: json["username"]);
 
-  Map<String, dynamic> toJson() => {
-        "title": title,
-        "link": link,
-      };
+  Map<String, dynamic> toJson() =>
+      {"title": title, "link": link, "username": username};
 }
--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:linktree_clone/entities/user_data.entity.dart';
 import 'package:linktree_clone/services/fetch_user_data.service.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeView extends StatefulWidget {
   @override
@@ -22,13 +23,38 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: _userdata == null ? 0 : _userdata!.length,
-        itemBuilder: (BuildContext context, int index) {
-          UserData userData = _userdata![index];
-          return Card(
-            child: Text(userData.title),
-          );
-        });
+
+    return Column(children: [
+      const CircleAvatar(
+        radius: 50,
+        backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/64553247'),
+      ),
+      SizedBox(height: 15,),
+      Expanded(
+        child: SizedBox(
+          child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: ListView.builder(
+                itemCount: _userdata == null ? 0 : _userdata!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  UserData userData = _userdata![index];
+                  return Card(
+              
+                    child: Column(
+                      children: [
+                        Text(userData.title),
+                        IconButton(
+                            onPressed: () {
+                              launch(userData.link);
+                            },
+                            icon: const Icon(Icons.arrow_forward_ios_rounded))
+                      ],
+                    ),
+                  );
+                }),
+          ),
+        ),
+      )
+    ],);
   }
 }
